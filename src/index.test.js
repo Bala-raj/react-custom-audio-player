73685a4ad5ed698e7adf6d1f5bc45749178340af
Johnson Abraham <johnson.thomson@anywhere.co>
2018-02-08 15:24:15 +0530

    updated test cases

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyComponent, {getFileName} from 'index';
+import MyComponent, {getFileName, getExtensionFromType} from 'index';
 
 describe('<index />', () => {
 
@@ -8,14 +8,32 @@ describe('<index />', () => {
         const div = document.createElement('div');
         ReactDOM.render(<MyComponent />, div);
     });
+
 });
 
-test('Retrieve filename by removing the extension if exists...', () => {
+describe('Filename manipulation', () => {
 
-    expect(getFileName('https://static1.squarespace.com/static/5547ab1ee4b0f235dbd475fc/5548268de4b0ec2ff50da4e9/5548dc18e4b0b0a763d06e89/1430838296099/09+Ashai+Mugam+-+ft.+Vidya+Vandana.mp3')).toBe('https://static1.squarespace.com/static/5547ab1ee4b0f235dbd475fc/5548268de4b0ec2ff50da4e9/5548dc18e4b0b0a763d06e89/1430838296099/09+Ashai+Mugam+-+ft.+Vidya+Vandana.');
+    test('Retrieves filename by removing file extension if exists...', () => {
 
-    expect(getFileName('9986c59b-4fd5-4925-a1f8-51fbadc6fd99_20180208T03:16_UTC.wav')).toBe('9986c59b-4fd5-4925-a1f8-51fbadc6fd99_20180208T03:16_UTC.');
-
-    expect(getFileName('9986c59b-4fd5-4925-a1f8-51fbadc6fd99_20180208T03:16_UTC')).toBe('9986c59b-4fd5-4925-a1f8-51fbadc6fd99_20180208T03:16_UTC');
+        expect(getFileName('https://static1.squarespace.com/static/5547ab1ee4b0f235dbd475fc/5548268de4b0ec2ff50da4e9/5548dc18e4b0b0a763d06e89/1430838296099/09+Ashai+Mugam+-+ft.+Vidya+Vandana.mp3')).toBe('https://static1.squarespace.com/static/5547ab1ee4b0f235dbd475fc/5548268de4b0ec2ff50da4e9/5548dc18e4b0b0a763d06e89/1430838296099/09+Ashai+Mugam+-+ft.+Vidya+Vandana');
     
-});
+        expect(getFileName('9986c59b-4fd5-4925-a1f8-51fbadc6fd99_20180208T03:16_UTC.wav')).toBe('9986c59b-4fd5-4925-a1f8-51fbadc6fd99_20180208T03:16_UTC');
+    
+        expect(getFileName('9986c59b-4fd5-4925-a1f8-51fbadc6fd99_20180208T03:16_UTC')).toBe('9986c59b-4fd5-4925-a1f8-51fbadc6fd99_20180208T03:16_UTC');
+    
+        expect(getFileName('blob_http___localhost_8887_b252abdc-926c-4f8d-82c0-e99671e7bf59'))
+        
+    });
+    
+    test('Returns filename with the passed filetype as extension', () => {
+    
+        expect('https://static1.squarespace.com/static/5547ab1ee4b0f235dbd475fc/5548268de4b0ec2ff50da4e9/5548dc18e4b0b0a763d06e89/1430838296099/09+Ashai+Mugam+-+ft.+Vidya+Vandana' + getExtensionFromType('audio/wav')).toBe('https://static1.squarespace.com/static/5547ab1ee4b0f235dbd475fc/5548268de4b0ec2ff50da4e9/5548dc18e4b0b0a763d06e89/1430838296099/09+Ashai+Mugam+-+ft.+Vidya+Vandana.wav');
+    
+        expect('blob_http___localhost_8887_b252abdc-926c-4f8d-82c0-e99671e7bf59' + getExtensionFromType('audio/ogg')).toBe('blob_http___localhost_8887_b252abdc-926c-4f8d-82c0-e99671e7bf59.ogg');
+    
+        expect('9986c59b-4fd5-4925-a1f8-51fbadc6fd99_20180208T03:16_UTC' + getExtensionFromType('audio/mpeg')).toBe('9986c59b-4fd5-4925-a1f8-51fbadc6fd99_20180208T03:16_UTC.mp3');
+    
+    });
+
+})
+
