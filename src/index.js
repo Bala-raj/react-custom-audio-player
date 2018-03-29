99fb87aec625d4e3481ce019c3998aa920e22793
Johnson Abraham <johnson.thomson@anywhere.co>
2018-03-29 15:15:49 +0530

    function that returns the name

--- src/index.js
+++ src/index.js
@@ -326,6 +326,11 @@ export default class AudioPlayer extends Component {
     });
   }
   
+  getName() {
+    return this.props.filename ? (this.props.type ? (extractFileName(this.props.filename) + getExtensionFromType(this.props.type)) : this.props.filename) : (this.props.type ? (extractFileName(this.proops.src) + getExtensionFromType(this.props.type)) : this.props.src);
+    alert(filename);
+  }
+  
   downloadAudio() {
     const filename = this.props.type ? (getFileName(this.props.src) + getExtensionFromType(this.props.type)) : this.props.src;
     if (isIEBrowser()) {
